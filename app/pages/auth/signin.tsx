e77ee68e6a0a9f347184cc80e598deabe5eece4f
williamstyronejr <williamsjtyrone@gmail.com>
2025-05-08 21:58:37 -0400

    Shadcn installs

---
+++ app/pages/auth/signin.tsx
@@ -0,0 +1,47 @@
+import Input from "~/components/Input";
+import Google from "~/icons/Google";
+
+export default function SigninPage() {
+  return (
+    <section className="flex flex-row flex-nowrap w-full items-center justify-center">
+      <div className="max-w-lg w-full px-4 py-6 bg-white rounded-md shadow-md">
+        <header className="">
+          <h1 className="font-bold text-xl py-4">Signin</h1>
+        </header>
+
+        <div className="flex flex-col flex-nowrap gap-8">
+          <div className="py-6">
+            <div className="border border-slate-200 rounded-md">
+              <div className="flex flex-row flex-nowrap gap-2 items-center justify-center py-2">
+                <Google className="w-8 h-8" />
+                <span>Signin with Google</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-row flex-nowrap items-center justify-center text-slate-400">
+            <hr className="grow" />
+            <span className="text-slate-400 px-4">or</span>
+            <hr className="grow" />
+          </div>
+
+          <div>
+            <div>
+              <Input name="email" type="text" label="Email" />
+              <Input name="password" type="password" label="Password" />
+            </div>
+
+            <div className="flex flex-row flex-nowrap justify-center w-full">
+              <button
+                type="submit"
+                className="bg-sky-500 hover:bg-sky-600 py-2 px-4 rounded-md text-white hover:cursor-pointer transition-colors"
+              >
+                Signin
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
