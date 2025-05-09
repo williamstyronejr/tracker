0ee71a036d3c6ca44b330dc74605533d8c03f6d8
williamstyronejr <williamsjtyrone@gmail.com>
2025-05-08 21:59:33 -0400

    Rename dashboard to habits

--- app/routes.ts
+++ app/routes.ts
@@ -6,13 +6,14 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-  layout("./layouts/LandingLayout.tsx", [index("pages/homepage.tsx")]),
-
-  route("signin", "pages/auth/signin.tsx"),
-  route("signup", "pages/auth/signup.tsx"),
+  layout("./layouts/LandingLayout.tsx", [
+    index("pages/homepage.tsx"),
+    route("signin", "pages/auth/signin.tsx"),
+    route("signup", "pages/auth/signup.tsx"),
+  ]),
 
   layout("./layouts/AppLayout.tsx", [
-    route("dashboard", "pages/dashboard/dashboard.tsx"),
+    route("habits/:mode?", "pages/habits/Habits.tsx"),
     route("settings/:panel?", "pages/settings/settings.tsx"),
   ]),
 ] satisfies RouteConfig;
