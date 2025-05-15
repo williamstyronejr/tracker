32376eb2968b1bdf668f07126ad6bd54dfd342bb
Tyrone Williams <williamsjtyrone@gmail.com>
2025-05-15 01:55:29 -0400

    Created helper function for session

--- app/lib/auth.client.ts
+++ app/lib/auth.client.ts
@@ -1,6 +1,15 @@
 import { createAuthClient } from "better-auth/react"; // make sure to import from better-auth/react
 
-export const { signIn, signUp, signOut, getSession, useSession } =
-  createAuthClient({
-    baseURL: "http://localhost:5173",
-  });
+export const { signIn, signUp, signOut, getSession } = createAuthClient({
+  baseURL: "http://localhost:5173",
+});
+
+export async function getSessionUser() {
+  const session = await getSession();
+
+  if (session.data && session.data.user) {
+    return session.data.user;
+  }
+
+  return null;
+}
